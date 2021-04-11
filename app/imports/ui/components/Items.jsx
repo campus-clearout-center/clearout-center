ba5883157513111ad796f2f3db8f0a7ad7f003a2
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-04-10 18:30:07 -1000

    Users are now able to see items in this branch

---
+++ app/imports/ui/components/Items.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Image, Card, Label, Button } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class Items extends React.Component {
+  render() {
+    return (
+      <Card centered>
+        <Image src={this.props.items.image} size='small' centered />
+        <Card.Content>
+          <Card.Header>{this.props.items.itemName}</Card.Header>
+          <Card.Meta>
+            <span>{this.props.items.firstName} {this.props.items.lastName}</span>
+          </Card.Meta>
+          <Card.Description>
+            {this.props.items.description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Card.Header>{this.props.items.price}<Button primary floated='right'>Offer</Button> </Card.Header>
+        </Card.Content>
+        <Card.Content extra>
+          <Label>
+            {this.props.items.label}
+          </Label>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+Items.propTypes = {
+  items: PropTypes.object.isRequired,
+};
+
+// Wrap this component in withRouter since we use the <Link> React Router element.
+export default withRouter(Items);
