4eea7d11a3b49adbecb1c245936e7f773fb7d05c
Benny <trieu6@hawaii.edu>
2021-04-13 09:20:45 -1000

    finished user-home-page and admin-home-page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+      <div className='uh-background'>
+        <Grid container centered stackable columns={3}>
 
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+          <Grid.Column textAlign='center'>
+            <Header as='h1' inverted>Welcome to Campus Clearout Center</Header>
+          </Grid.Column>
+        </Grid>
+      </div>
     );
   }
 }
