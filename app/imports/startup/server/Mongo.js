299011b03c37a71359e0394813fe4c38e4ac7144
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-04-14 11:49:11 -1000

    checkpoint

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Item } from '../../api/item/Item';
 
 /* eslint-disable no-console */
 
@@ -14,5 +15,18 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+function addItem(data) {
+  console.log(`  Adding: ${data.lastName} (${data.owner})`);
+  Item.collection.insert(data);
+}
+
+// Initialize the StuffsCollection if empty.
+if (Item.collection.find().count() === 0) {
+  if (Meteor.settings.defaultItem) {
+    console.log('Creating default item.');
+    Meteor.settings.defaultItem.map(data => addItem(data));
   }
 }
